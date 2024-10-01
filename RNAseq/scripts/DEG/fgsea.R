4059418f97b03960fab4b87f72876297b066ccc1
lavauxt <lavauxt@github.com>
2024-10-01 11:21:22 +0200

    update fgsea

--- RNAseq/scripts/DEG/fgsea.R
+++ RNAseq/scripts/DEG/fgsea.R
@@ -6,10 +6,15 @@
 # Sources for reference
 # https://stephenturner.github.io/deseq-to-fgsea/
 # https://github.com/hamidghaedi/Enrichment-Analysis
+# https://yulab-smu.top/biomedical-knowledge-mining-book/enrichplot.html
 
 # Guide for gmt sets
 # https://www.gsea-msigdb.org/gsea/msigdb/human/collections.jsp#H
 # https://data.broadinstitute.org/gsea-msigdb/msigdb/release/7.5.1/
+
+message('Start fgsea script')
+
+# Load library
 suppressPackageStartupMessages({
   library(tidyverse)
   library(reshape2)
@@ -18,110 +23,108 @@ suppressPackageStartupMessages({
   library(htmlwidgets)
   library(maditr)
   library(ggplot2)
+  library(optparse)
+
   library(fgsea)
   library(DT)
   library(ComplexHeatmap)
-  library(optparse)
+  library(DOSE)
+  library(clusterProfiler)
+  library(enrichplot)
 })
 
 ######Parsing input options and setting defaults########
-# Load DE analyse from DESeq2
-option_list<-list(
-	make_option('--input',default='data', help='DE analysis file',dest='datafile'),
-	make_option('--output',default='results', help='Folder were to save the results',dest='resultfolder'),
-	make_option('--pathway',default='', help='Pathways to use, gmt file',dest='pathway')
+# Load DE analysis from DESeq2
+option_list <- list(
+  make_option('--input', default = 'data', help = 'DE analysis file', dest = 'datafile'),
+  make_option('--output', default = 'results', help = 'Folder where to save the results', dest = 'resultfolder'),
+  make_option('--pathway', default = '', help = 'Pathways to use, gmt file', dest = 'pathway')
 )
-opt<-parse_args(OptionParser(option_list=option_list))
+opt <- parse_args(OptionParser(option_list = option_list))
 
-FolderOutput=opt$resultfolder
-DataInput=opt$datafile
-Pathways=opt$pathway
+FolderOutput <- opt$resultfolder
+DataInput <- opt$datafile
+Pathways <- opt$pathway
 
-dir.create(FolderOutput)
+dir.create(FolderOutput, showWarnings = FALSE)
 
-# Load gmt
+message('Load gmt')
 gmt_file_name <- file_path_sans_ext(basename(Pathways))
 
-# Load datas
+message('Load DE results')
 res <- read_tsv(paste(DataInput))
 
-# Load the pathways into a named list
-pathways.hallmark <- gmtPathways(paste(Pathways))
+message('Load the pathways for GSEA')
+pathways.GSEA <- read.gmt(paste(Pathways))
 
-# We use log fold change cf https://support.bioconductor.org/p/129277/
-res2 <- res %>% 
-  dplyr::select(gene, log2FoldChange) %>% 
-  na.omit() %>% 
-  distinct() %>% 
-  group_by(gene) %>% 
-  summarize(log2FoldChange=mean(log2FoldChange))
-res2
+message('Load the pathways for fgsea')
+pathways.fgsea <- gmtPathways(paste(Pathways))
 
+message('Convert the res data frame to a named vector for GSEA')
+gene_list <- res$log2FoldChange
+names(gene_list) <- res$gene
+
+message('Remove NA values and sort the gene list in decreasing order')
+gene_list <- na.omit(gene_list)
+gene_list <- sort(gene_list, decreasing = TRUE)
+
+message('Perform GSEA analysis')
+gsea_results <- GSEA(gene_list, TERM2GENE = pathways.GSEA, pvalueCutoff = 0.05)
+
+message('Prepare data for fgsea')
+res2 <- res %>%
+  dplyr::select(gene, log2FoldChange) %>%
+  na.omit() %>%
+  distinct() %>%
+  group_by(gene) %>%
+  summarize(log2FoldChange = mean(log2FoldChange))
 ranks <- deframe(res2)
-head(ranks, 20)
 
-# Perform fgsea analysis
-fgseaRes <- fgseaMultilevel(pathways=pathways.hallmark, stats=ranks)
+message('Perform fgsea analysis')
+fgseaRes <- fgseaMultilevel(pathways = pathways.fgsea, stats = ranks)
 
+message('Show in an HTML table')
 fgseaResTidy <- fgseaRes %>%
   as_tibble() %>%
   arrange(desc(NES))
-
-# Show in a html table
-datatable_object <- fgseaResTidy %>% 
-  dplyr::select(-leadingEdge, -ES) %>% 
-  arrange(padj) %>% 
+datatable_object <- fgseaResTidy %>%
+  dplyr::select(-leadingEdge, -ES) %>%
+  arrange(padj) %>%
   DT::datatable()
-# Save in html
-output_file <- paste("GSEA_Table_with", gmt_file_name, ".html")
-saveWidget(datatable_object, file = paste(FolderOutput, output_file))
+output_file <- paste0("GSEA_Table_with_", gmt_file_name, ".html")
+saveWidget(datatable_object, file = paste(FolderOutput, output_file, sep = "/"))
 
-# Filtered results by padj value
-fgseaResTidy_filtered <- fgseaResTidy %>% filter(padj < 0.001)
-
-ggplot(fgseaResTidy_filtered, aes(reorder(pathway, NES), NES)) +
-  geom_col(aes(fill=padj<0.05)) +
-  coord_flip() +
-  labs(x="Pathway", y="Normalized Enrichment Score",
-       title= paste("Results for GMT file: ", gmt_file_name)) + 
-  theme_minimal()
-
-# Table by gene
-gene.in.pathway <- pathways.hallmark %>% 
-  enframe("pathway", "gene") %>% 
-  unnest(cols = c(gene)) %>% 
-  inner_join(res, by="gene")
-
-# Filter pathways based on padj < 0.001
-filtered_pathways <- fgseaResTidy %>%
-  filter(padj < 0.001) %>%
-  pull(pathway)
-
-# Subset the pathways.hallmark to include only the filtered pathways
-filtered_pathways_hallmark <- pathways.hallmark[filtered_pathways]
-
-# Plot function
-plot_function <- function(gene_set_name) {
-  gene_set <- filtered_pathways_hallmark[[gene_set_name]]
-  
-  # Create the enrichment plot
-  plot <- plotEnrichment(pathway = gene_set, stats = ranks) +
-    ggtitle(paste("Enrichment Plot for: ", gene_set_name))
-  
-  output_file <- paste0(FolderOutput, gsub(" ", "_", gene_set_name), ".pdf")
-  
-  # Save the plot
-  ggsave(output_file, plot=plot, width=8, height=6)
+message('Iterate over each pathway and save GSEA plots with gseaplot2')
+gene_set_ids <- gsea_results@result$ID
+for (i in gene_set_ids) {
+    # Check for valid enrichment score in gseaplot2
+    if (!is.null(gsea_results@result[i, ]) && !any(is.na(gsea_results@result[i, ]$ES))) {
+      p <- tryCatch(
+        {
+          pathway_name <- gsub("_", " ", gsea_results@result[gsea_results@result$ID == i, "Description"])
+          gseaplot2(gsea_results, geneSetID = i, title = as.character(pathway_name))
+        },
+        error = function(e) {
+          message(paste("Error generating gseaplot2 for pathway:", i, "Skipping..."))
+          return(NULL)
+        }
+      )
+    }
+    
+  # Save plots if valid
+  if (!is.null(p)) {
+    # Save as PDF
+    pdf_file <- paste0(FolderOutput, "/GSEA_plot_", gmt_file_name, "_", i, ".pdf")
+    ggsave(filename = pdf_file, plot = p, width = 8, height = 6)
+    
+    # Save as TIFF
+    tiff_file <- paste0(FolderOutput, "/GSEA_plot_", gmt_file_name, "_", i, ".tiff")
+    ggsave(filename = tiff_file, plot = p, width = 8, height = 6, device = "tiff")
+  } else {
+    message(paste("Skipping pathway:", i, "due to invalid results or plotting error."))
+  }
 }
 
-# Apply the function to each gene set in pathways.hallmark
-lapply(names(filtered_pathways_hallmark), plot_function)
-
-output_file <- paste("GSEA_Table_with", gmt_file_name, ".pdf")
-pdf(paste(FolderOutput, output_file))
-plotGseaTable(pathways.hallmark[fgseaRes$pathway[fgseaRes$padj < 0.001]], ranks, fgseaRes, 
-                gseaParam=0.5)
-dev.off()
-
+message('All done')
 ### Output the versions of all tools used in the analysis
 sessionInfo()
